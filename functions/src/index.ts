d8c51a814fa028bc403a7182a6b8ad82d1aa0615
허민 <heomin02@naver.com>
2024-01-23 14:23:09 +0900

    creat: : 업로드 페이지까지

---
+++ functions/src/index.ts
@@ -0,0 +1,16 @@
+// import {onRequest} from "firebase-functions/v2/https";
+// import * as logger from "firebase-functions/logger";
+
+// const functions = require('firebase-functions');
+
+// exports.bigben = functions.https.onRequest((req, res) => {
+//   const hours = (new Date().getHours() % 12) + 1  // London is UTC + 1hr;
+//   res.status(200).send(`<!doctype html>
+//     <head>
+//       <title>Time</title>
+//     </head>
+//     <body>
+//       ${'BONG '.repeat(hours)}
+//     </body>
+//   </html>`);
+// });
